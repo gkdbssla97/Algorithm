c7eea26d2920ac0acc76ea2eb0b2838b6a6d0ac9
Hayoon <gkdbssla97@gmail.com>
2021-07-28 17:38:50 +0900

    Union_Find

--- Union_Find.c
+++ Union_Find.c
@@ -1,4 +1,44 @@
-//
-// Created by 하윤 on 2021/07/28.
-//
+#include <stdio.h>
 
+int getParent(int parent[], int x) {
+    if(parent[x] == x) return x;
+    return parent[x] = getParent(parent, parent[x]);
+}
+
+// 각 부모 노드 합치기
+void unionParent(int parent[], int a, int b) {
+    a = getParent(parent, a);
+    b = getParent(parent, b);
+    if(a < b) parent[b] = a;
+    else parent[a] = b;
+}
+
+// 같은 부모 노드를 가지는지 확인
+int findParent(int parent[], int a, int b) {
+    a = getParent(parent, a);
+    b = getParent(parent, b);
+    if(a == b) return 1;
+    else return 0;
+}
+
+int main(void) {
+    int parent[11];
+    for(int i = 1; i<= 10; i++)
+        parent[i] = i;
+
+    unionParent(parent,1, 2);
+    unionParent(parent,2, 3);
+    unionParent(parent,3, 4);
+    unionParent(parent,5, 6);
+    unionParent(parent,6, 7);
+    unionParent(parent,7, 8);
+
+    printf("1과 5는 연결되어 있나요? %d \n", findParent(parent, 1, 5));
+    unionParent(parent,1,5);
+    printf("1과 5는 연결되어 있나요? %d \n", findParent(parent, 1, 5));
+    printf("4과 5는 연결되어 있나요? %d \n", findParent(parent, 4, 5));
+
+    printf("4의 부모노드는? %d \n", getParent(parent,4));
+
+    return 0;
+}
